a4d963ebd6f93a761a257f37f6e69db4a0b55068
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2021-12-10 00:06:48 +0100

    initial changes

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("YES")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("NO")
+})
+radio.setGroup(1)
+let canVote = true
+basic.forever(function () {
+	
+})
