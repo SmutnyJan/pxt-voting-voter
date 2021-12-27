34c799eb9bd73bcddf9057ef71526cb4171d2dff
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2021-12-27 21:59:55 +0100

    added support for four voting options

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue("YES", control.deviceSerialNumber())
+    radio.sendValue("A", control.deviceSerialNumber())
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendValue("C", control.deviceSerialNumber())
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendValue("NO", control.deviceSerialNumber())
+    radio.sendValue("B", control.deviceSerialNumber())
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendValue("D", control.deviceSerialNumber())
 })
 radio.setGroup(1)
 basic.forever(function () {
