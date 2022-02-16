55acd2606a7141405627e642653e13c3cd4d6bad
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-02-16 15:16:27 +0100

    better ux

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendValue("A", control.deviceSerialNumber())
+    basic.showString("A")
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendValue("C", control.deviceSerialNumber())
+    basic.showString("C")
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendValue("B", control.deviceSerialNumber())
+    basic.showString("B")
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radio.sendValue("D", control.deviceSerialNumber())
+    basic.showString("D")
 })
 radio.setGroup(1)
 basic.forever(function () {
