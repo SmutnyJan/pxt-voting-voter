18528b1e8a37f784b9f40e8af1f0200dc652f805
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-03-14 22:35:17 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue("A", control.deviceSerialNumber())
+    radio.sendString("A")
     basic.showString("A")
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendValue("C", control.deviceSerialNumber())
+    radio.sendString("C")
     basic.showString("C")
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendValue("B", control.deviceSerialNumber())
+    radio.sendString("B")
     basic.showString("B")
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    radio.sendValue("D", control.deviceSerialNumber())
+    radio.sendString("D")
     basic.showString("D")
 })
 radio.setGroup(1)
+radio.setTransmitSerialNumber(true)
 basic.forever(function () {
 	
 })
